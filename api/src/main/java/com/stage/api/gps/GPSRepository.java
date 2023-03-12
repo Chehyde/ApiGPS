849eff5658cd83ae0e395f84943e40eadfdbd860
Chehyde <quentin.crosnier@gmail.com>
2023-03-12 17:49:53 +0100

    .

--- api/src/main/java/com/stage/api/gps/GPSRepository.java
+++
@@ -1,7 +0,0 @@
-package com.stage.api.gps;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GPSRepository extends CrudRepository<GPS, Integer> { }
